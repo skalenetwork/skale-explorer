43284ad6208cc2d25a89c6aba5e1519719ad9859
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2022-08-08 21:15:08 +0300

    Add scraper for cloudflare

--- admin/statistics/utils.py
+++ admin/statistics/utils.py
@@ -1,9 +1,10 @@
-import requests
+import cfscrape
 from admin import GAS_PRICES_FILEPATH
 
 
 def download_gas_prices():
+    scraper = cfscrape.create_scraper()
     url = 'https://etherscan.io/chart/gasprice?output=csv'
-    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    response = scraper.get(url, headers={'User-Agent': 'Mozilla/5.0'})
     with open(GAS_PRICES_FILEPATH, 'w') as f:
         f.write(response.text)
