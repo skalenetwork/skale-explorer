fe40507ccfe9e4144982693447f7b85b76cb947f
DmytroNazarenko <dmytro.nazarenko@nure.ua>
2023-03-10 02:38:46 +0200

    Add decorators

--- admin/agent.py
+++ admin/agent.py
@@ -1,6 +1,7 @@
 import logging
 import os
 from datetime import datetime
+from functools import wraps
 from threading import Thread
 from time import sleep
 import psycopg2
@@ -19,12 +20,26 @@ from admin.utils.logger import init_logger
 logger = logging.getLogger(__name__)
 
 
+def daemon(delay=60):
+    def actual_decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            logger.info(f'Initiating {func.__name__}')
+            while True:
+                func(*args, **kwargs)
+                sleep(delay)
+        return wrapper
+    return actual_decorator
+
+
+@daemon
 def check_explorer_status():
     schains = get_all_names()
     for schain_name in schains:
         check_explorer_for_schain(schain_name)
 
 
+@daemon
 def collect_statistics():
     if not is_statistic_updated():
         schains = get_all_names()
@@ -36,6 +51,7 @@ def collect_statistics():
             logger.warning(f'Collecting failed: {e}')
 
 
+@daemon
 def update_gas_prices():
     if not is_gas_prices_updated():
         schains = get_all_names()
@@ -47,36 +63,15 @@ def update_gas_prices():
             update_gas_prices_time(current_date)
 
 
-def run_checker():
-    logger.info('Initiating explorer checker')
-    while True:
-        check_explorer_status()
-        sleep(60)
-
-
-def run_stats_collector():
-    logger.info('Initiating stats collector')
-    while True:
-        collect_statistics()
-        sleep(60)
-
-
-def run_gas_prices_update():
-    logger.info('Initiating gas prices update')
-    while True:
-        update_gas_prices()
-        sleep(60)
-
-
 def main():
     assert os.path.isfile(ABI_FILEPATH), "ABI not found"
     if not os.path.isfile(EXPLORERS_META_DATA_PATH):
         create_meta_file()
     create_tables()
 
-    Thread(target=run_checker, daemon=True, name='Monitor1').start()
-    Thread(target=run_gas_prices_update, daemon=True, name='Monitor2').start()
-    Thread(target=run_stats_collector, daemon=True, name='Monitor3').start()
+    Thread(target=check_explorer_status, daemon=True, name='explorers-checker').start()
+    Thread(target=update_gas_prices, daemon=True, name='gas-price-checker').start()
+    Thread(target=collect_statistics, daemon=True, name='stats-collector').start()
     while True:
         sleep(1)
 
