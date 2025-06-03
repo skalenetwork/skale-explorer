f77f91940c19d249ecd7b32fb550e3c4e0f98917
Vadim Yavorsky <yavrsky@gmail.com>
2025-06-03 23:22:35 +0100

    Fix typo

--- admin/core/endpoints.py
+++ admin/core/endpoints.py
@@ -187,7 +187,7 @@ def get_schain_endpoint(schain_name, ws=False):
     endpoints = endpoints_for_schain(schains_internal_contract, nodes_contract, schain_id)
     for node in endpoints['nodes']:
         if ws:
-            endpoint = (node['ws_endpoin_ipt'] if HTTP_ENDPOINT == 'true'
+            endpoint = (node['ws_endpoint_ip'] if HTTP_ENDPOINT == 'true'
                         else node['wss_endpoint_domain'])
         else:
             endpoint = (node['http_endpoint_ip'] if HTTP_ENDPOINT == 'true'
